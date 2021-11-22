3bbe222109b3e22dd6a12c134cd3b81a0267079d
Christian Payne <christianpayne522@gmail.com>
2021-11-21 20:55:25 -0800

    Minor changes to the header and button name.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,20 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <header className="text-gray-400 bg-gray-900 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
+      <header className="text-gray-400 bg-gray-900 body-font fixed w-screen z-10">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-white">
+          <a className="cursor-pointer flex title-font font-medium items-center mb-4 md:mb-0" href="">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="mr-3 w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg> */}
-            <span className="ml-3 text-xl">Frank The Plumber</span>
+            <span className="text-xl p-2">Frank The Plumber</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-white">About</a>
-            <a className="mr-5 hover:text-white">Services</a>
+            <a className="cursor-pointer mr-5" href="about">About</a>
+            <a className="cursor-pointer mr-5" href="services">Services</a>
           </nav>
-          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Contact
+          <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+            (714) 482-5144
             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -151,7 +152,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <button className="flex mx-auto mt-16 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+          <button className="flex mx-auto mt-16 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Contact</button>
         </div>
       </section>
 
