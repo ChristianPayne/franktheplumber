19916e53f33579eea3ef5ff2d93030c3bc00b902
Christian Payne <christianpayne522@gmail.com>
2022-03-12 18:42:32 -0800

    Removed some spacing on the bottom of the home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,13 +24,13 @@ export default function Index () {
       <div className="relative w-full h-96">
         <Image src="https://dummyimage.com/400x600/666666/fff.jpg" layout='fill' objectFit="cover" alt="WorkImage1"/>
         <div className="w-full h-1/2 absolute bottom-0 bg-gradient-to-t from-main-1"></div>
-        <h1 className=" text-5xl font-bold absolute left-8 bottom-8">&quot;No job <br/>too small.&quot;</h1>
+        <h1 className=" text-5xl font-semibold absolute left-8 bottom-8">&quot;No job <br/>too small.&quot;</h1>
       </div>
 
       <div className="content">
         <p className="mb-12">Whether it be a flooded bathroom or a leaky faucet, Frank will stop by to inspect what needs to be done. His priority is making you feel comfortable calling him for any issue that might arise.</p>
         <Button name="Contact" href="/contact" className="mb-12 bg-accent-2 text-main-1"/>
-        <p className="mb-12">Licensed plumber proudly servicing the Southern California Region for over 25 years.</p>
+        <p>Licensed plumber proudly servicing the Southern California Region for over 25 years.</p>
       </div>
     </div>
   )
